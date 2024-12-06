6e6b1f93132c9fa054621d44cc40d3d6f52d28c9
Detachment207 <190797478+Detachment207@users.noreply.github.com>
2024-12-06 15:23:57 +1100

    Added AusSof

---
+++ Other-Core-Mods/AusSof/mod.cpp
@@ -0,0 +1,17 @@
+name = "AUSSOF_LITE_mas";
+picture = "AUSSOF_ico.paa";
+logoSmall = "AUSSOF_logo_small.paa";
+logo = "AUSSOF_logo.paa";
+logoOver = "AUSSOF_logo.paa";
+action = "";
+tooltipOwned = "AUS-SOF";
+dlcColor[] = 
+{
+	0.23, // Red
+	0.39, // Green
+	0.30, // Blue
+	1 // Alpha
+};
+overview = "A wide variety of Australian-SOF units for Arma3 covering all operational environments and tactics";
+hideName = 0;
+hidePicture = 0;
